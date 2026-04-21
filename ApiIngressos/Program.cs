6f35befcf9d74a76d7b3241ab3d4f40f08da6724
Vinícius Schonfelder <vini.schonfelder@gmail.com>
2026-04-21 16:22:25 -0300

    Refactor Program.cs for database connection and endpoints
    Refactor Program.cs to include connection string and register DbConnectionFactory. Added endpoint mappings for eventos, usuarios, and cupons.

--- ApiIngressos/Program.cs
+++ ApiIngressos/Program.cs
@@ -1,9 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString =
+    builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddSingleton(new DbConnectionFactory(connectionString));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
-app.Run();
-
+// chama os endpoints separados
+app.MapEventosEndpoints();
 app.MapUsuariosEndpoints();
 app.MapCuponsEndpoints();
+
+app.Run();
